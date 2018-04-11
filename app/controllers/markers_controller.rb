cd3a649ec73f8ee8ae53991a2d7f55311c29fef7
gluk <gluk.main+github@gmail.com>
2018-04-11 23:40:45 +0300

    FEATURE: Ability to add markers
    Form + processing (route, params handling)
    Markers are now ordered: the last added is the first

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,5 +1,16 @@
 class MarkersController < ApplicationController
   def index
-    @markers = Marker.all
+    @markers = Marker.all.order(created_at: :desc)
+  end
+
+  def create
+    Marker.create(lat: allowed_params[:lat], lng: allowed_params[:lng])
+    redirect_to request.referer
+  end
+
+  private
+
+  def allowed_params
+    params.require(:marker).permit(:lat, :lng)
   end
 end
