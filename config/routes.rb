acd59ebe75933ad65c7606ae2d8c748dcb322d53
gluk <gluk.main+github@gmail.com>
2018-04-25 01:28:37 +0300

    TASK1: Showing map on /task1 instead of /task1/markers
    Routes adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/task1' do
     resources :markers, only: [:index, :create]
+    root 'markers#index'
   end
 
   # get 'freelancers/index'
